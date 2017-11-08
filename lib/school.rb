8c6734ba75ab9d67077496b5887df3d41afa2aa1
Chris Cardle <cdcardle@gmail.com>
2017-11-08 20:14:19 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,8 +7,8 @@ class School
   end
 
   def add_student(student, grade)
-    roster[grade] = [] unless roster[grade]
-    roster[grade] << student
+    @roster[grade] = [] unless @roster[grade]
+    @roster[grade] << student
   end
 
   def grade(grade)
